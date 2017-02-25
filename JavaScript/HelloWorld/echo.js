910e6d0ee0f6601edf58a8b70cfdbbf74dfa41db
ogawa-yu <badm.y.ogawa@gmail.com>
2017-02-26 02:13:09 +0900

    Javaspript : echo serverの作成

---
+++ JavaScript/HelloWorld/echo.js
@@ -0,0 +1,14 @@
+
+// netモジュールを読み込み、インスタンスを作成
+var net = require('net');
+
+// serverのイベントハンドラ定義
+net.createServer(function(socket) {
+
+    // ソケット経由でメッセージ送出
+    socket.write('This is RLS Texh News Echo Server\r\n');
+    // データを受信したら、そのままメッセージとして送出
+    socket.on('data', function(data) {
+        socket.write(data)
+    });
+}).listen(1337, '127.0.0.1');
