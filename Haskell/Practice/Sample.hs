ba61fe86e9d45748caa687ffb45ff35c7fb27f52
ogawa-yu <badm.y.ogawa@gmail.com>
2017-03-02 00:37:03 +0900

    Haskell : 関数適用のサンプル

---
+++ Haskell/Practice/Sample.hs
@@ -0,0 +1,15 @@
+
+digits :: Int -> Int
+digits = length . show
+
+square :: Num a => a -> a
+square = (^2)
+
+square' :: Num a => a -> a
+square' x = x ^ 2
+
+ultimate :: Int -> String
+ultimate 42 = "人生、宇宙、すべての答え"
+ultimate n = show n
+
+
