ccb7a041314d1ce16803bd6f69b8aa0589024936
ogawa-yu <badm.y.ogawa@gmail.com>
2017-03-04 10:39:25 +0900

    タブ文字を空白に置換する関数

--- Haskell/Practice/Sample.hs
+++ Haskell/Practice/Sample.hs
@@ -12,4 +12,16 @@ ultimate :: Int -> String
 ultimate 42 = "人生、宇宙、すべての答え"
 ultimate n = show n
 
+maybeToList :: Maybe a -> [a]
+maybeToList Nothing = []
+maybeToList (Just a) = [a]
 
+listToMaybe :: [a] -> Maybe a
+listToMaybe [] = Nothing
+listToMaybe (a:as) = Just a
+
+deeping :: String -> String
+deeping []             = []
+deeping ('\t':'\t':xs) = "  " ++ deeping xs
+deeping ('\t':xs)      = " " ++ deeping xs
+deeping (s:xs)         = [s] ++ deeping xs
