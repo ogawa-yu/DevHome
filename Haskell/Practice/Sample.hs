8b14e3ea35287379131b5fd4efb19aad7aea543c
ogawa-yu <badm.y.ogawa@gmail.com>
2017-03-04 11:08:51 +0900

    Haskell : ダメなパターンマッチの例と、検出方法をメモ

--- Haskell/Practice/Sample.hs
+++ Haskell/Practice/Sample.hs
@@ -22,6 +22,16 @@ listToMaybe (a:as) = Just a
 
 deeping :: String -> String
 deeping []             = []
-deeping ('\t':'\t':xs) = "  " ++ deeping xs
-deeping ('\t':xs)      = " " ++ deeping xs
+deeping ('\t':xs)      = "    " ++ deeping xs
 deeping (s:xs)         = [s] ++ deeping xs
+
+-- よくないパターンマッチの例
+-- 網羅的に定義できていない。
+-- 0, 1, 2以外の整数を関数の引数に渡すと
+-- 実行時エラーになる
+-- こうした定義不足は、 ghci起動時に "-W"オプションをつけると
+-- コンパイラが検出してくれる
+--nonExhaustive :: Int -> Int
+--nonExhaustive 0 = 0
+--nonExhaustive 1 = 1
+--nonExhaustive 2 = 2
