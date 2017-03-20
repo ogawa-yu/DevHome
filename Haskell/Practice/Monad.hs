efcbfc2c45beb76f6ef0e2ee81f6f1a20b4e2c1e
ogawa-yu <badm.y.ogawa@gmail.com>
2017-03-20 17:06:28 +0900

    Haskell : モナド演習

---
+++ Haskell/Practice/Monad.hs
@@ -0,0 +1,46 @@
+class BoolLike a where
+    fromBoolLike :: a -> Bool -- 真偽値のようなものから実際の真偽値への変換
+
+instance BoolLike Int where
+    fromBoolLike = (0 /=)
+
+instance BoolLike (Maybe a) where
+    fromBoolLike Nothing = False
+    fromBoolLike (Just _) = True
+
+instance BoolLike Bool where
+    fromBoolLike x = x
+
+data A = A
+data B = B
+
+class ToInt a where
+    toInt :: a -> Int
+
+instance ToInt A where
+    toInt _ = 1
+
+instance ToInt B where
+    toInt _ = 2
+
+add :: ToInt x => x -> x -> Int
+add x y = toInt x + toInt y
+
+add' :: (ToInt x, ToInt y) => x -> y -> Int
+add' x y = toInt x + toInt y
+
+square :: Integer -> Maybe Integer
+square n
+    | 0 <= n    = Just (n * n)
+    | otherwise = Nothing
+
+squareRoot :: Integer -> Maybe Integer
+squareRoot n 
+    | 0 <= n    = squareRoot' 1
+    | otherwise = Nothing
+    where 
+        squareRoot' x
+            | n > x * x = squareRoot' (x + 1)
+            | n < x * x = Nothing
+            | otherwise = Just x
+
