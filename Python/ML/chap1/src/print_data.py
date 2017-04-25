ef61438182658060e862c1edfaa281513619e00e
ogawa-yu <badm.y.ogawa@gmail.com>
2017-04-26 01:19:18 +0900

    Python: 散布図の描画

--- Python/ML/chap1/src/print_data.py
+++ Python/ML/chap1/src/print_data.py
@@ -1,9 +1,38 @@
 import numpy as np
 import scipy as sp
+import matplotlib.pyplot as plt
 
-data = sp.genfromtxt("../data/web_t.tsv", delimiter="\t")
+def error(f, x, y) :
+    return sp.sum((f(x)-y)**2)
+
+data = sp.genfromtxt("../data/web_traffic.tsv", delimiter="\t")
+#data = sp.genfromtxt("../data/web_t1.tsv", delimiter="\t")
 
 print(data[:10])
 
 print(data.shape)
 
+x = data[:,0]
+y = data[:,1]
+x = x[~sp.isnan(y)]
+y = y[~sp.isnan(y)]
+
+fp1, residuals, rank, sv, rcond = sp.polyfit(x, y, 1, full=True)
+print("Model parameters: %s" % fp1)
+print(residuals)
+f1 = sp.poly1d(fp1)
+print(error(f1, x, y))
+
+fx = sp.linspace(0, x[-1], 1000)
+plt.plot(fx, f1(fx), linewidth=4)
+plt.legend(["d=%i" % f1.order],loc="upper left")
+
+plt.scatter(x, y)
+plt.title("Web traffic over the last month")
+plt.xlabel("Time")
+plt.ylabel("Hits/hour")
+plt.xticks([w*7*24 for w in range(10)],
+        ['week %i' %w for w in range(10)])
+plt.autoscale(tight=True)
+plt.grid()
+plt.show()
