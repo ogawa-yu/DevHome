fd76edee5bc74a89a4a34f3429a5b9ceff62aaf4
ogawa-yu <badm.y.ogawa@gmail.com>
2017-04-30 00:23:35 +0900

    Python : データ量増やした。

--- Python/ML/chap1/src/np_perf.py
+++ Python/ML/chap1/src/np_perf.py
@@ -1,11 +1,11 @@
 
 import timeit
 
-normal_py_sec = timeit.timeit('sum(x * x for x in xrange(1000))',
-                                number = 10000)
+normal_py_sec = timeit.timeit('sum(x * x for x in xrange(1000))', number=10000)
+
 native_np_sec = timeit.timeit('sum(na * na)',
-                                setup="import numpy as np; na=np.arange(1000)",
-                                number=10000)
+                              setup="import numpy as np; na=np.arange(1000)",
+                              number=10000)
 good_np_sec = timeit.timeit('na.dot(na)',
                             setup="import numpy as np; na = np.arange(1000)",
                             number=10000)
