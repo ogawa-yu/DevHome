9f7e151c1002083692d6e66b1bb8f4c9fbef1d25
ogawa-yu <badm.y.ogawa@gmail.com>
2017-05-04 01:02:15 +0900

    Python: シグモイド関数の計算・描画

---
+++ Python/DL/Dev/neural/sigmoid.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def myplot(x):
+    plt.plot(x)
+    plt.show()
+
+y = sigmoid(np.arange(1, 10, 0.1))
+myplot(y)
