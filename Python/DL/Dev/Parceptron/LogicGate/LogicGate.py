2dd8e6ad90aef5d9d3374f1f787d01707d7fc320
ogawa-yu <badm.y.ogawa@gmail.com>
2017-05-03 21:58:44 +0900

    Python: 重みとバイアスの計算を改良

--- Python/DL/Dev/Parceptron/LogicGate/LogicGate.py
+++ Python/DL/Dev/Parceptron/LogicGate/LogicGate.py
@@ -1,7 +1,10 @@
+import numpy as np
+
+# 論理ゲートの演算を行うクラス
 class LogicGate:
     def __init__(self, w1, w2, theta):
-        self.w1 = w1
-        self.w2 = w2
-        self.theta = theta
+        self.w = np.array([w1, w2])
+        self.bias = -theta
     def output(self, x1, x2):
-        return (self.w1*x1 + self.w2*x2) > self.theta
+        x = np.array([x1, x2])
+        return (np.sum(self.w*x) + self.bias) > 0
