8c6a0ed840a85926299703dac3a0a815b13a22b4
ogawa-yu <badm.y.ogawa@gmail.com>
2017-05-04 02:30:54 +0900

    Python: 実装を洗練

--- Python/DL/Dev/neural/thirdlayernetwrk.py
+++ Python/DL/Dev/neural/thirdlayernetwrk.py
@@ -2,37 +2,33 @@ import numpy as np
 from sigmoid import sigmoid
 from identity import identity_function
 
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]]) 
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+    return network
+
 def transport_signal(X, W, B):
     return np.dot(X, W) + B
 
-# 入力層
-X1 = np.array([1.0, 0.5])
-# 入力層-中間層の重み
-W1 = np.array([[0.1, 0.3, 0.5],
-               [0.2, 0.4, 0.6]])
-# 入力層のバイアス
-B1 = np.array([0.1,
-               0.2,
-               0.3])
-# 中間層1
-A1 = transport_signal(X1,W1,B1)
-# 活性化した中間ノード
-Z1 = sigmoid(A1)
+def forward(network, X):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
-# 中間層1-中間層2の重み
-W2 = np.array([[0.1, 0.4],
-               [0.2, 0.5],
-               [0.3, 0.6]])
-# 中間層1のバイアス
-B2 = np.array([0.1, 0.2])
-#中間層2
-A2 = transport_signal(Z1, W2, B2)
-Z2 = sigmoid(A2)
+    a1 = transport_signal(X, W1, b1)
+    z1 = sigmoid(a1)
 
-#中間層2-出力層の重み
-W3 = np.array([[0.1, 0.3],
-               [0.2, 0.4]])
-B3 = np.array([0.1, 0.2])
+    a2 = transport_signal(z1, W2, b2)
+    z2 = sigmoid(a2)
 
-Y  = identity_function(transport_signal(A2, W3, B3))
-print(Y)
+    a3 = transport_signal(z2, W3, b3)
+    return identity_function(a3)
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+print(y)
