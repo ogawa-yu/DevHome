80f68caf91699f1eb44fffddc4e3961544fd2046
ogawa-yu <badm.y.ogawa@gmail.com>
2017-05-04 13:52:49 +0900

    Python: MNISTを用いたニューラルネットワークの構築

--- Python/DL/Dev/neural/softmax.py
+++ Python/DL/Dev/neural/softmax.py
@@ -1,7 +1,13 @@
 import numpy as np
 # ソフトマックス関数
 # 演算した結果のベクトルは、要素の和が1になる性質がある
+# すなわち各要素は1に正規化された値と変換することができる
 def softmax(x):
-    c = np.max(x) # オーバーフローを避けるため、expへ渡す引数を小さくする
-    exp_x = np.exp(x - c)
-    return exp_x / np.sum(exp_x)
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T 
+
+    x = x - np.max(x) # オーバーフロー対策
+    return np.exp(x) / np.sum(np.exp(x))
