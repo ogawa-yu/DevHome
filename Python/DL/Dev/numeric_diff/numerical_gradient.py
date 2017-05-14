87ba386e5d1477cc4478f571e3bd7f41a41b7741
ogawa-yu <badm.y.ogawa@gmail.com>
2017-05-14 19:23:47 +0900

    Ruby: ファイルを引っ越し

--- Python/DL/Dev/numeric_diff/numerical_gradient.py
+++ Python/DL/Dev/numeric_diff/numerical_gradient.py
@@ -1,5 +1,7 @@
 import numpy as np
 from numeric_diff import sample_function_2d
+#import matplotlib.pyplot as plt
+import matplotlib.pylab as plt
 
 def gradient(f, x):
     h = 1e-4
@@ -21,11 +23,28 @@ def gradient(f, x):
 
 def gradient_descent(f, init_x, larning_rate=0.01, step_num=100):
     x = init_x
+    x_history = []
+
     for i in range(step_num):
+        x_history.append(x.copy())
+
         grad = gradient(f, x)
         x -= larning_rate * grad
-    return x
+    return x, np.array(x_history)
 
 if (__name__ == '__main__'):
     init_x = np.array([-3.0, 4.0])
-    print (gradient_descent(sample_function_2d, init_x=init_x, larning_rate=0.1, step_num=100))
+    x, x_history = gradient_descent(sample_function_2d, init_x=init_x, larning_rate=1e-1, step_num=100)
+
+    plt.plot( [-5, 5], [0,0], '--b')
+    plt.plot( [0,0], [-5, 5], '--b')
+    plt.plot(x_history[:,0], x_history[:,1], 'o')
+
+    plt.xlim(-3.5, 3.5)
+    plt.ylim(-4.5, 4.5)
+    plt.xlabel("X0")
+    plt.ylabel("X1")
+
+    print(x)
+
+    plt.show()
