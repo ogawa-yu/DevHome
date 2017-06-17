d375df517946b11e2cac2fecbf83960f47a3a374
ogawa-yu <badm.y.ogawa@gmail.com>
2017-06-18 01:07:33 +0900

    Java: 命令型による記法

--- Java/Functional/introduction/fpij/Cities.java
+++ Java/Functional/introduction/fpij/Cities.java
@@ -2,17 +2,8 @@ import java.util.*;
 
 class Cities {
     public static void main(String[] args) {
-        boolean found = false;
         List<String> cities = new ArrayList<>();
         cities.add("Chicago");
-
-        for (String city : cities) {
-            if (city.equals("Chicago")) {
-                found = true;
-                break;
-            }
-        }
-
         System.out.println("Found Chicago?:" + cities.contains("Chicago"));
     }
 
---
+++ Java/Functional/introduction/fpij/DiscountImperative.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.math.BigDecimal;
+
+class DiscountImperative {
+    public static void main(String[] args) {
+        final List<BigDecimal> PRICES = 
+            Arrays.asList(
+                    new BigDecimal("10"), new BigDecimal("30"), new BigDecimal("17"),
+                    new BigDecimal("20"), new BigDecimal("15"), new BigDecimal("18"),
+                    new BigDecimal("45"), new BigDecimal("12")
+            );
+        BigDecimal total = BigDecimal.ZERO;
+        for (BigDecimal price : PRICES) {
+            if (price.compareTo(BigDecimal.valueOf(20)) > 0) {
+                total = total.add(price.multiply(BigDecimal.valueOf(0.9)));
+            }
+        }
+
+        System.out.println("Total prices is " + total.toString());
+    }
+}
