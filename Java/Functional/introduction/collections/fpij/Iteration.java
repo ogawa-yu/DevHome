53f749c2c86a3ac2440e41164ead97ac6c3147bc
ogawa-yu <badm.y.ogawa@gmail.com>
2017-06-18 17:45:31 +0900

    Java : 型推論でラムダ式の型宣言を省略できる。

--- Java/Functional/introduction/collections/fpij/Iteration.java
+++ Java/Functional/introduction/collections/fpij/Iteration.java
@@ -2,9 +2,8 @@ import java.util.*;
 
 class Iteration {
     public static void main(String[] args) {
-        final List<String> friends = 
-            Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
-        friends.forEach((final String name) -> System.out.println(name));
+        Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott")
+        .forEach((name) -> System.out.println(name));
     }
 
 }
