481e21d0a364f29d3b0974090fcaccae843b7ccd
ogawa-yu <badm.y.ogawa@gmail.com>
2017-06-18 17:50:14 +0900

    Java : メソッド参照に変更

--- Java/Functional/introduction/collections/fpij/Iteration.java
+++ Java/Functional/introduction/collections/fpij/Iteration.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Iteration {
     public static void main(String[] args) {
         Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott")
-        .forEach((name) -> System.out.println(name)); // ラムダ式は型推論してくれるが、その場合final修飾子はつかないので注意
+        .forEach(System.out::println);
     }
 
 }
