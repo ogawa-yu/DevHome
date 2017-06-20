5be62a416b36d6661d3a1486fff1a664b96b99f2
ogawa-yu <badm.y.ogawa@gmail.com>
2017-06-21 00:59:13 +0900

    Java : 戻り値がprimitiveなやつの特殊Functionがあったので、そっち使う。

--- Java/Functional/introduction/collections/fpij/PickElementMulti.java
+++ Java/Functional/introduction/collections/fpij/PickElementMulti.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.function.Function;
-import java.util.function.BiFunction;
+import java.util.function.ToLongBiFunction;
 import java.util.stream.Stream;
 
 class PickElementMulti {
@@ -10,12 +10,12 @@ class PickElementMulti {
         final List<String> friends = Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
         final List<String> editors = Arrays.asList("Brian", "Jackie", "Jone", "Mike");
         final List<String> comrades = Arrays.asList("Kate", "Ken", "Nick", "Paula", "Zach");
-        final BiFunction<List<String>, String, Long> countNameWith =
+        final ToLongBiFunction<List<String>, String> countNameWith =
             (names, letter) -> names.stream().filter(name -> name.startsWith(letter)).count();
             
-        System.out.println(countNameWith.apply(friends, "N"));
-        System.out.println(countNameWith.apply(friends, "B"));
-        System.out.println(countNameWith.apply(editors, "N"));
-        System.out.println(countNameWith.apply(comrades, "N"));
+        System.out.println(countNameWith.applyAsLong(friends, "N"));
+        System.out.println(countNameWith.applyAsLong(friends, "B"));
+        System.out.println(countNameWith.applyAsLong(editors, "N"));
+        System.out.println(countNameWith.applyAsLong(comrades, "N"));
     }
 }
