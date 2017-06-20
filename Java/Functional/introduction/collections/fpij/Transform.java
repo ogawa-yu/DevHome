810cf945422f7e5605fec63cba95a8fcefe1c973
ogawa-yu <badm.y.ogawa@gmail.com>
2017-06-20 23:48:53 +0900

    Java : メソッド参照

--- Java/Functional/introduction/collections/fpij/Transform.java
+++ Java/Functional/introduction/collections/fpij/Transform.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Transform {
     public static void main(String[] args) {
         final List<String> friends = Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
-        friends.stream().map(name -> name.toUpperCase())
+        friends.stream().map(String::toUpperCase)
                         .forEach(name -> System.out.print(name + " "));
         System.out.println(); // 改行のため
     }
