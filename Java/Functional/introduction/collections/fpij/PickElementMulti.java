268b14495b482394541df63b9229c5abc1ff2774
ogawa-yu <badm.y.ogawa@gmail.com>
2017-06-21 00:01:21 +0900

    Java : ラムダ式の再利用その2. ラムダ式を変数に束縛

--- Java/Functional/introduction/collections/fpij/PickElementMulti.java
+++ Java/Functional/introduction/collections/fpij/PickElementMulti.java
@@ -2,17 +2,29 @@ import java.util.*;
 
 class PickElementMulti {
 
+    private final java.util.function.Predicate<String> pred_;
+
     public static void main(String[] args) {
         final List<String> friends = Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
         final List<String> editors = Arrays.asList("Brian", "Jackie", "Jone", "Mike");
         final List<String> comrades = Arrays.asList("Kate", "Ken", "Nick", "Paula", "Zach");
-
-        System.out.println(countStartN(friends));
-        System.out.println(countStartN(editors));
-        System.out.println(countStartN(comrades));
+        final java.util.function.Predicate<String> startWithN = name -> name.startsWith("N");
+        
+        System.out.println(count(startWithN).of(friends));
+        System.out.println(count(startWithN).of(editors));
+        System.out.println(count(startWithN).of(comrades));
     }
 
-    static long countStartN(final List<String> names) {
-        return names.stream().filter(name -> name.startsWith("N")).count();
+    private PickElementMulti(final java.util.function.Predicate<String> pred) {
+        pred_ = pred;
+    }
+
+    static public PickElementMulti count(final java.util.function.Predicate<String> pred) {
+        PickElementMulti finder = new PickElementMulti(pred);
+        return finder;
+    }
+
+    long of(final List<String> names) {
+        return names.stream().filter(pred_).count();
     }
 }
