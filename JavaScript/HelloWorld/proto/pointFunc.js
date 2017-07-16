36c8530e72c412d1003f4389b2aaa2ed98df684c
ogawa-yu <badm.y.ogawa@gmail.com>
2017-07-16 15:30:15 +0900

    Javascript: 既存のコンストラクタ関数のprototype継承とオーバーライド

--- JavaScript/HelloWorld/proto/pointFunc.js
+++ JavaScript/HelloWorld/proto/pointFunc.js
@@ -10,3 +10,24 @@ var p = new Point(1, 2);
 print(p.x);
 print(p.y);
 print(p.getNorm());
+
+function Point3D(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+}
+
+// 継承っぽいこと
+Point3D.prototype = new Point();
+// オーバーライドっぽいこと
+Point3D.prototype.getNorm = function() {
+    return Math.sqrt(this.x * this.x 
+        + this.y * this.y
+        + this.z * this.z);
+}
+
+var p3 = new Point3D(1, 2, 3);
+print(p3.x);
+print(p3.y);
+print(p3.z);
+print(p3.getNorm());
