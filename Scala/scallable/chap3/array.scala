8dcf2acc0db6ff9c1f736ea45c73cc2e343c6689
ogawa-yu <badm.y.ogawa@gmail.com>
2017-08-05 16:51:38 +0900

    Scala: 最初のバージョンと等価な書き方(コンパイルされるとこう変換される)

--- Scala/scallable/chap3/array.scala
+++ Scala/scallable/chap3/array.scala
@@ -1,7 +1,7 @@
 val greetString = new Array[String](3)
-greetString(0) = "Hello"
-greetString(1) = ", "
-greetString(2) = "World!"
-for (i <- 0 to 2)
-    print(greetString(i))
+greetString.update(0, "Hello")
+greetString.update(1, ", ")
+greetString.update(2, "World!")
+for (i <- 0.to(2))
+    print(greetString.apply(i))
 println()
