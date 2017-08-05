a60c1fbb86b97c70be76c25da1c1d87ac02b0279
ogawa-yu <badm.y.ogawa@gmail.com>
2017-08-05 17:10:16 +0900

    Scala: Listの初期化は::とNilで数珠つなぎもできる

--- Scala/scallable/chap3/list.scala
+++ Scala/scallable/chap3/list.scala
@@ -4,5 +4,5 @@ val oneToFour = oneTwo ::: threeFour
 println(oneTwo + " and " + threeFour + " ware not mutated")
 println("Thus, " + oneToFour + " is a new list")
 
-val oneTwoThrree = 3 :: oneTwo
+val oneTwoThrree = 1 :: 2 :: 3 :: Nil
 println(oneTwoThrree)
