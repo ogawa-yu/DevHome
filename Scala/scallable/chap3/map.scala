efe8cbde62484ff88c01b68ffc1f8eeb34270967
ogawa-yu <badm.y.ogawa@gmail.com>
2017-08-06 23:46:13 +0900

    Scala: mapの利用

---
+++ Scala/scallable/chap3/map.scala
@@ -0,0 +1,12 @@
+import scala.collection.mutable
+val treasureMap = mutable.Map[String, String]()
+treasureMap += ("foo" -> "Go to island.")
+treasureMap += ("bar" -> "Find big X on Ground.")
+treasureMap += ("boo" -> "Dig.")
+println(treasureMap("bar"))
+
+val romanNumeral = Map(
+    1 -> "I", 2 -> "II", 3 -> "III", 4 -> "IV", 5 -> "V"
+)
+
+println(romanNumeral(3))
