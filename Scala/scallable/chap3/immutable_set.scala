14f51463d96dc1c364666625ed7f81a0ed6456ba
ogawa-yu <badm.y.ogawa@gmail.com>
2017-08-06 23:07:00 +0900

    Scala: HashSet

--- Scala/scallable/chap3/immutable_set.scala
+++ Scala/scallable/chap3/immutable_set.scala
@@ -1,3 +1,9 @@
+import scala.collection.immutable.HashSet
+
 var jetSet = Set("Boeing", "Airbus")
 jetSet += "Lear"
 println(jetSet.contains(args(0)))
+
+val hashSet = HashSet("Tomatoes", "Chilies")
+println(hashSet + "Coriander")
+println(jetSet)
