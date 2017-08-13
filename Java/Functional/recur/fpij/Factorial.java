14aeac68641b2bf123d6fa4731de10f53871d23a
ogawa-yu <badm.y.ogawa@gmail.com>
2017-08-13 20:46:35 +0900

    Java : 末尾再帰最適化を階乗計算に適用 　　　スタックオーバーフローを解決

--- Java/Functional/recur/fpij/Factorial.java
+++ Java/Functional/recur/fpij/Factorial.java
@@ -1,18 +1,22 @@
 import java.util.*;
 
 class Factorial {
-    public static long factorialRec(final long number) {
+    public static long factorial(final long number) {
+        return factorialRec(1, number).invoke();
+    }
+
+    public static TailCall<Long> factorialRec(final long factorial, final long number) {
         if (number == 1) {
-            return number;
+            return TailCalls.done(factorial);
         }
         else {
-            return number * factorialRec(number - 1);
+            return TailCalls.call(() -> factorialRec(factorial * number, number - 1));
         }
     }
 
     public static void main(String[] args) {
         try {
-            System.out.println(Factorial.factorialRec(new Long(args[0])));
+            System.out.println(Factorial.factorial(new Long(args[0])));
         } catch (StackOverflowError e) {
             System.out.println(e);
         }
