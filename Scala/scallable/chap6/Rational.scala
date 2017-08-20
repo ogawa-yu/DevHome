6c4a13f11ef1fe5c8fc45ea185a3519e49900e60
ogawa-yu <badm.y.ogawa@gmail.com>
2017-08-21 01:03:21 +0900

    Scala: 有理数クラスの実装 分子と分母を正規化するように変更

--- Scala/scallable/chap6/Rational.scala
+++ Scala/scallable/chap6/Rational.scala
@@ -1,8 +1,8 @@
 class Rational(n: Int, d: Int) {
     require(d != 0)
-    override def toString = n + "/" + d
-    val number: Int = n
-    val denom: Int = d
+    private val g = gcd(n.abs, d.abs)
+    val number: Int = n / g
+    val denom: Int = d / g
     def this(n: Int) = this(n, 1)
     def add(that: Rational): Rational = {
         return new Rational(
@@ -14,4 +14,8 @@ class Rational(n: Int, d: Int) {
     def lessThan(that: Rational) = this.number * that.denom < this.denom * that.number
 
     def max(that: Rational) = if(lessThan(that)) that else this
+
+    override def toString = this.number + "/" + this.denom
+
+    private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 }
