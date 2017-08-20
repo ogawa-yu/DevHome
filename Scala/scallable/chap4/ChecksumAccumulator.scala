2e8072a2e75936afc2a8809480ecad925c02430c
ogawa-yu <badm.y.ogawa@gmail.com>
2017-08-20 15:32:10 +0900

    Scala: シングルトンオブジェクトとコンパニオン

---
+++ Scala/scallable/chap4/ChecksumAccumulator.scala
@@ -0,0 +1,28 @@
+import scala.collection.mutable
+
+// class Accumulatorのコンパニオンオブジェクト
+object ChecksumAccumulator {
+    private val cache = mutable.Map.empty[String, Int]
+    def calculate (s: String): Int = {
+        if (cache.contains(s)) {
+            cache(s)
+        } else {
+            val acc = new ChecksumAccumulator
+            for (c <- s) 
+                acc.add(c.toByte)
+            val cs = acc.checksum()
+            cache += (s -> cs)
+            cs
+        }
+    }
+}
+
+class ChecksumAccumulator {
+    private var sum_ = 0
+
+    def add(b: Byte) = sum_ += b
+
+    def checksum() = ~(sum_ & 0xFF) + 1
+}
+
+println(ChecksumAccumulator.calculate(args(0)))
