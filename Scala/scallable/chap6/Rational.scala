b4f67c866d0e7271e15ce33def1992e230ada4c4
ogawa-yu <badm.y.ogawa@gmail.com>
2017-08-21 00:42:48 +0900

    Scala: 有理数クラス実装 フィールド追加と単純な加算処理

--- Scala/scallable/chap6/Rational.scala
+++ Scala/scallable/chap6/Rational.scala
@@ -1,4 +1,13 @@
 class Rational(n: Int, d: Int) {
     require(d != 0)
     override def toString = n + "/" + d
+    val number: Int = n
+    val denom: Int = d
+
+    def add(that: Rational): Rational = {
+        return new Rational(
+            number * that.denom + that.number * denom,
+            denom * that.denom
+        )
+    }
 }
