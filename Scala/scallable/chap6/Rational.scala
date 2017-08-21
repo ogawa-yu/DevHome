4b0201643680fdb083c6227979d489a839405b8b
ogawa-yu <badm.y.ogawa@gmail.com>
2017-08-21 22:59:36 +0900

    Scala: 有理数クラスの四則演算

--- Scala/scallable/chap6/Rational.scala
+++ Scala/scallable/chap6/Rational.scala
@@ -13,15 +13,25 @@ class Rational(n: Int, d: Int) {
     def +(that: Int): Rational =
         new Rational(number + that * denom, denom)
 
-    def *(that: Rational): Rational = {
+    def -(that: Rational): Rational = {
         return new Rational(
-            number * that.number,
+            number * that.denom - that.number * denom,
             denom * that.denom
         )
     }
+    def -(that: Int): Rational =
+        this + new Rational(that) * (-1)
+        
+    def *(that: Rational): Rational = 
+        new Rational(number * that.number,denom * that.denom)
     def *(that: Int) : Rational =
         new Rational(number * that, denom)
 
+    def /(that: Rational): Rational =
+        new Rational(number * that.denom, denom * that.number)
+    def /(that: Int) : Rational =
+        new Rational(number, denom * that)
+    
     def lessThan(that: Rational) = this.number * that.denom < this.denom * that.number
 
     def max(that: Rational) = if(lessThan(that)) that else this
