d4c9bbf0367049a35d7a3fab03ada9a05e8d6a35
ogawa-yu <badm.y.ogawa@gmail.com>
2017-09-02 23:24:32 +0900

    Scala: 値としての関数による重複の削減

--- Scala/scallable/chap9/fileMatcher.scala
+++ Scala/scallable/chap9/fileMatcher.scala
@@ -2,16 +2,13 @@ import java.io._
 
 object FileMatcher extends App {
     def filesHere = (new java.io.File(".")).listFiles
-    def filesEnding(query: String) =
-        for (file <- filesHere; if file.getName.endsWith(query))
+    def filesMatching(query: String, matcher: (String, String) => Boolean) =
+        for (file <- filesHere; if matcher(file.getName, query))
             yield file
-    def filesContaining(query: String) =
-        for (file <- filesHere; if file.getName.contains(query))
-            yield file
-    def filesRegex(query: String) =
-        for (file <- filesHere; if file.getName.matches(query))
-            yield file
-    
+    def filesEnding(query: String) = filesMatching(query, _.endsWith(_))
+    def filesContaining(query: String) = filesMatching(query, _.contains(_))
+    def filesRegex(query: String) = filesMatching(query, _.matches(_))
+
     println("==>endswith")
     filesEnding(args(0)).foreach(println)
     println("==>contains")
