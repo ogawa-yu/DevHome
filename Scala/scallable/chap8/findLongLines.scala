c620191301892336399eca98bd98f1c53e7c2570
ogawa-yu <badm.y.ogawa@gmail.com>
2017-09-02 19:26:00 +0900

    Scala: オブジェクト指向的なメソッドの例

---
+++ Scala/scallable/chap8/findLongLines.scala
@@ -0,0 +1,5 @@
+object FindLongLines extends App {
+    val width = args(0).toInt
+    for (arg <- args.drop(1))
+        LongLines.processFile(arg, width)
+}
---
+++ Scala/scallable/chap8/longLines.scala
@@ -0,0 +1,14 @@
+import scala.io.Source
+
+object LongLines {
+    def processFile(fileName: String, width: Int) = {
+        val source = Source.fromFile(fileName)
+        for (line <- source.getLines())
+            processLine(fileName, width, line)
+    }
+
+    private def processLine(fileName: String, width: Int, line: String) = {
+        if (line.length > width)
+            println(fileName + ": " + line.trim)
+    }
+}
