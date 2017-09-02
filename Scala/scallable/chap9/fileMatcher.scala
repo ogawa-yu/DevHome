6cb558e0bf9970af3c6042e3d9450df58fdf823a
ogawa-yu <badm.y.ogawa@gmail.com>
2017-09-02 23:25:39 +0900

    Scala: クロージャで さらに冗長性削減

--- Scala/scallable/chap9/fileMatcher.scala
+++ Scala/scallable/chap9/fileMatcher.scala
@@ -2,12 +2,12 @@ import java.io._
 
 object FileMatcher extends App {
     def filesHere = (new java.io.File(".")).listFiles
-    def filesMatching(query: String, matcher: (String, String) => Boolean) =
-        for (file <- filesHere; if matcher(file.getName, query))
+    def filesMatching(query: String, matcher: (String) => Boolean) =
+        for (file <- filesHere; if matcher(file.getName))
             yield file
-    def filesEnding(query: String) = filesMatching(query, _.endsWith(_))
-    def filesContaining(query: String) = filesMatching(query, _.contains(_))
-    def filesRegex(query: String) = filesMatching(query, _.matches(_))
+    def filesEnding(query: String) = filesMatching(query, _.endsWith(query))
+    def filesContaining(query: String) = filesMatching(query, _.contains(query))
+    def filesRegex(query: String) = filesMatching(query, _.matches(query))
 
     println("==>endswith")
     filesEnding(args(0)).foreach(println)
