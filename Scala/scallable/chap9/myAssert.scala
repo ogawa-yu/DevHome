3539f3cbb889236be3a3fa2b919efe4f2909f5b6
ogawa-yu <badm.y.ogawa@gmail.com>
2017-09-03 13:52:28 +0900

    Scala: 名前渡しパラメータの利用で、評価式のみ記述できるようにする、

--- Scala/scallable/chap9/myAssert.scala
+++ Scala/scallable/chap9/myAssert.scala
@@ -1,10 +1,10 @@
 object MyAssert extends App {
     val assertionEnabled = true
-    def myAssert(msg: String, predicate: () => Boolean) = 
-        if (assertionEnabled && !predicate())
+    def byNameAssert(msg: String, predicate: => Boolean) = 
+        if (assertionEnabled && !predicate)
             throw new AssertionError(msg)
 
-    myAssert("5 greater than 3", () => 5 > 3)
+    byNameAssert("5 greater than 3", 5 > 3)
 
-    myAssert("6 is a odd", () => 6 % 2 == 1)
+    byNameAssert("6 is a odd", 6 % 2 == 0)
 }
