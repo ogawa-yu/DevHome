52a984c9b29a59ae4bb449e000a8a0fcbd7ad867
ogawa-yu <badm.y.ogawa@gmail.com>
2017-09-04 01:03:23 +0900

    Scala: トレイトのメソッドもoverrideできる

--- Scala/scallable/chap12/frog.scala
+++ Scala/scallable/chap12/frog.scala
@@ -2,4 +2,7 @@ class Animal
 trait HasLegs
 class Frog extends Animal with Philosophical with HasLegs {
   override def toString = "green"
+  override def philosophize() = {
+    println("It ain't easy being " + toString + "!")
+  }
 }
