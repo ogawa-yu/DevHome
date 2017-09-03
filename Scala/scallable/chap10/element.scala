18bccc7820858b2369575852b3aaa7e345718f90
ogawa-yu <badm.y.ogawa@gmail.com>
2017-09-03 16:28:34 +0900

    Scala: 2Dレイアウトライブラリの作成 シングルトンオブジェクトによるファクトリの実装

--- Scala/scallable/chap10/element.scala
+++ Scala/scallable/chap10/element.scala
@@ -3,12 +3,21 @@ abstract class Element {
     def height: Int = contents.length
     def width: Int = if (height == 0) 0 else contents(0).length
     def above(that: Element): Element = 
-        new ArrayElement(this.contents ++ that.contents)
+        Element.elem(this.contents ++ that.contents)
     def beside(that: Element): Element = 
-        new ArrayElement(
+        Element.elem(
             for(
                 (line1, line2) <- this.contents zip that.contents
             ) yield line1 + line2
         )
     override def toString = contents mkString "\n"
+}
+
+object Element {
+    def elem(contents: Array[String]) =
+        new ArrayElement(contents)
+    def elem(s: String) =
+        new LineElement(s)
+    def elem(ch: Char, width: Int, height: Int) =
+        new UniformElement(ch, width, height)
 }
--- Scala/scallable/chap10/testElement.scala
+++ Scala/scallable/chap10/testElement.scala
@@ -1,32 +1,32 @@
 object ElementTest extends App {
     def arrayElementTest = { 
-        val arrayElem = new ArrayElement(Array("hello", "world"))
+        val arrayElem = Element.elem(Array("hello", "world"))
         arrayElem.contents.foreach(println)
         assert(arrayElem.height == 2, "要素数は2")
         assert(arrayElem.width == 5, "'hello'のlengthは5")
     }
     def lineElementTest = {
-        val lineElem = new LineElement("hello world")
+        val lineElem = Element.elem("hello world")
         lineElem.contents.foreach(println)
         assert(lineElem.width == 11)
         assert(lineElem.height == 1)
     }
     def uniformElementTest = {
-        val unifElem = new UniformElement('A', 2, 3)
+        val unifElem = Element.elem('A', 2, 3)
         unifElem.contents.foreach(println)
         assert(unifElem.width == 2)
         assert(unifElem.height == 3)
     }
     def test_above = {
-        val elem1 = new LineElement("I am")
-        val elem2 = new LineElement("cat.")
-        val elem3 = new LineElement("there is no name yet")
+        val elem1 = Element.elem("I am")
+        val elem2 = Element.elem("cat.")
+        val elem3 = Element.elem("there is no name yet")
         assert(Array("I am", "cat.", "there is no name yet") == elem1.above(elem2).above(elem3).contents)
     }
     def test_beside = {
-        val elem1 = new LineElement("I am")
-        val elem2 = new LineElement(" cat.")
-        val elem3 = new LineElement(" there is no name yet")
+        val elem1 = Element.elem("I am")
+        val elem2 = Element.elem(" cat.")
+        val elem3 = Element.elem(" there is no name yet")
 
         assert("I am cat. there is no name yet" == elem1.beside(elem2).beside(elem3).toString)
     }
