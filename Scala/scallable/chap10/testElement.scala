81c708ee64be7a5586b54c7c91d1cd222501c220
ogawa-yu <badm.y.ogawa@gmail.com>
2017-09-03 15:25:34 +0900

    Scala: 2Dレイアウトライブラリの作成 単体テストっぽくした。

--- Scala/scallable/chap10/testElement.scala
+++ Scala/scallable/chap10/testElement.scala
@@ -1,4 +1,6 @@
 object ElementTest extends App {
-    val arrayElem = new ArrayElement(Array("a", "b", "c"))
+    val arrayElem = new ArrayElement(Array("hello", "world"))
     arrayElem.contents.foreach(println)
+    assert(arrayElem.height == 2, "要素数は2")
+    assert(arrayElem.width == 5, "'hello'のlengthは5")
 }
