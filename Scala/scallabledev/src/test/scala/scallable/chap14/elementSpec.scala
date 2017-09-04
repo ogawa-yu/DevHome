dea0b2a1961545490b11579f9fdb433b9c1d1903
ogawa-yu <badm.y.ogawa@gmail.com>
2017-09-04 20:08:31 +0900

    Scala: LineElementのテストケース追加

--- Scala/scallabledev/src/test/scala/scallable/chap14/elementSpec.scala
+++ Scala/scallabledev/src/test/scala/scallable/chap14/elementSpec.scala
@@ -16,4 +16,22 @@ class ElementSpec extends FlatSpec with Matchers {
         val ele = elem('x', -2, 3)
       }
     }
+  "A LineElement" should
+    "have a width equal to the passed value" in {
+      val ele = elem("zzzzz")
+      ele.width should be (5)
+    }
+    it should "have a height equal to the passed value" in {
+      val ele = elem("zzzzz")
+      ele.height should be (1)
+    }
+  "A ArrayElement" should 
+    "have a width equal to the passed value" in {
+      val ele = elem(Array("a", "b", "c"))
+      ele.width should be (1)
+    }
+    it should "have a height equal to the passed value" in {
+      val ele = elem(Array("a", "b", "cd"))
+      ele.height should be (3)
+    }
 }
