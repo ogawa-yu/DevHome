84f7d9907cefb52105a0b5fb2c2dd6277de274e1
ogawa-yu <badm.y.ogawa@gmail.com>
2017-09-04 17:19:14 +0900

    Scala: トレイトと実装のStackable modifications

--- Scala/scallabledev/src/main/scala/scallable/chap12/intQueue.scala
+++ Scala/scallabledev/src/main/scala/scallable/chap12/intQueue.scala
@@ -1,6 +1,22 @@
-import scala.collection.mutable.ArrayBuffer
 abstract class IntQueue {
-  private val buf = new ArrayBuffer[Int]
-  def get(): Int = buf.
+  def get(): Int
   def put(x: Int)
+}
+
+trait Doubling extends IntQueue {
+  abstract override def put(x: Int) = {
+    super.put(2 * x)
+  }
+}
+
+trait Incrementing extends IntQueue {
+  abstract override def put(x: Int) = {
+    super.put(x + 1)
+  }
+}
+
+trait Filtering extends IntQueue {
+  abstract override def put(x: Int) = {
+    if (x >= 0) super.put(x)
+  }
 }
