06608a5edd4c73bd14006cf00637eeccb26bf343
ogawa-yu <badm.y.ogawa@gmail.com>
2017-09-04 21:05:17 +0900

    Scala: ケースクラスとパターンマッチ

--- Scala/scallabledev/src/main/scala/scallable/chap15/expr.scala
+++ Scala/scallabledev/src/main/scala/scallable/chap15/expr.scala
@@ -1,4 +1,11 @@
-abstract class Expr 
+abstract class Expr {
+    def simplifyTop(expr: Expr) =
+        expr match {
+            case UnOp("-", UnOp("-", e)) => e
+            case BinOp("+", e, Number(0)) => e
+            case BinOp("*", e, Number(1)) => e
+        }
+}
 case class Var(name: String) extends Expr
 case class Number(num: Double) extends Expr
 case class UnOp(operator: String, arg: Expr) extends Expr
--- Scala/scallabledev/src/test/scala/scallable/chap15/exprSpec.scala
+++ Scala/scallabledev/src/test/scala/scallable/chap15/exprSpec.scala
@@ -12,4 +12,17 @@ class ExprSpec extends FlatSpec with Matchers {
     it should "not equal to the passed value" in {
       Var("test") != Var("test dayo") should be (true)
     }
+  "A Expr" should
+    "as same as tha passed value when negative multiplied negative one" in {
+      val num = Number(3.0)
+      num.simplifyTop(UnOp("-", UnOp("-", num))) == num should be (true)
+    }
+    it should "as same as the passed value when zero plused" in {
+      val num = Number(3.0)
+      num.simplifyTop(BinOp("+", num, Number(0))) == num should be (true)
+    }
+    it should "as same as the passed value when one multiplied" in {
+      val num = Number(3.0)
+      num.simplifyTop(BinOp("*", num, Number(1))) == num should be (true)
+    }
 } 
