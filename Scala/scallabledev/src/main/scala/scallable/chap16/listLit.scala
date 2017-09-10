655a84e04b5438b78af301591ca088f03c2ad567
ogawa-yu <badm.y.ogawa@gmail.com>
2017-09-10 15:11:54 +0900

    Scala: cons(::)とNilによるListの構築

--- Scala/scallabledev/src/main/scala/scallable/chap16/listLit.scala
+++ Scala/scallabledev/src/main/scala/scallable/chap16/listLit.scala
@@ -1,14 +1,12 @@
 object LiteralList extends App {
-  val fruit: List[String] = List("apple", "banana", "grape")
-  val nums: List[Int] = List(2, 5, 9, 13)
-  val diag3: List[List[Int]] =
-    List(
-      List(1, 2, 3),
-      List(4, 5, 6),
-      List(7, 8 ,9)
-    )
-  val empty: List[Nothing] = List()
-  
+  val fruit = "apple" :: ("banana" :: ("grape" :: Nil))
+  val nums = 2 :: (5 :: (9 :: (7 :: Nil)))
+  val diag3 =
+    (1 :: (2 :: (3 :: Nil))) ::
+    (4 :: (5 :: (6 :: Nil))) ::
+    (7 :: (8 :: (9 :: Nil))) :: Nil
+  val empty = Nil
+
   fruit.foreach(e =>print(e + " "))
   println("")
   nums.foreach(e =>print(e + " "))
