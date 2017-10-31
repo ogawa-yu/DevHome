15c09b449c318a45861185dbe2a5cc9f632684d6
ogawa-yu <badm.y.ogawa@gmail.com>
2017-11-01 00:56:20 +0900

    Python: 文字列(各種文字列関数)

---
+++ Python/Starting/chap2/string_join.py
@@ -0,0 +1,43 @@
+print('abc' + 'def')
+
+print("ABC" "DEF")
+
+print('abc' 'def')
+
+print("ABC" + "DEF")
+
+# 5回繰り返す
+str_ = 'abc' * 5
+print(str_)
+
+#文字列の長さを取得する
+print(len(str_))
+
+#キーワードによる文字列の分割
+media = 'CD,DVD,MD,LD,BD'
+mediaList = media.split(',')
+print(mediaList)
+
+#文字列リストの結合
+print(':'.join(mediaList))
+
+# 全て小文字
+print(media.lower())
+
+#title case
+print(media.title())
+
+#swap case
+print(media.title().swapcase())
+
+# capitalize
+print(media.capitalize())
+
+#centering
+print(media.center(20))
+
+#right align
+print(media.rjust(20))
+
+#left align
+print(media.ljust(20))
