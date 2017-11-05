ea962599e4ef73ec72ae386d5f979c7e17e6c7f1
ogawa-yu <badm.y.ogawa@gmail.com>
2017-11-06 00:36:45 +0900

    Python: マングリングによる属性の擬似private化

--- Python/Starting/chap6/src/person.py
+++ Python/Starting/chap6/src/person.py
@@ -1,12 +1,12 @@
 class Person:
     def __init__(self, input_name):
-        self.hidden_name = input_name
+        self.__name = input_name
     
     def get_name(self):
-        return self.hidden_name
+        return self.__name
 
     def set_name(self, input_name):
-        self.hidden_name = input_name
+        self.__name = input_name
     name = property(get_name, set_name)
 
 class EmailPerson(Person):
@@ -20,6 +20,8 @@ class EmailPerson(Person):
 if __name__ == '__main__':
     p = Person('test')
     print(p.name)
+    # "__"を属性名の前につけると"_Class名"を勝手に修飾する(マングリング)
+    print(p._Person__name)
 
     e = EmailPerson('test', 'mail')
     print(e.name)
