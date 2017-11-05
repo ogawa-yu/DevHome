c1497672713f61b1d1760cb9b88a10b7a43ac69c
ogawa-yu <badm.y.ogawa@gmail.com>
2017-11-05 23:11:33 +0900

    Python: 自作の例外クラスのthrow

--- Python/Starting/chap4/exception.py
+++ Python/Starting/chap4/exception.py
@@ -1,7 +1,7 @@
-import unittest
+import unittest as ut
 
-class TestException(unittest.TestCase):
-    def test_try_except(self):
+class TestException(ut.TestCase):
+    def test_try_except_basis(self):
         testee = [1, 2, 3]
         pos = 4
         try:
@@ -10,5 +10,36 @@ class TestException(unittest.TestCase):
         except:
             pass
 
+    def test_try_except_specified_execption(self):
+        testee = [1, 2, 3]
+        pos = 4
+
+        try:
+            testee[pos]
+            assert False
+        except IndexError as err:
+            print('occured index error')
+            pass
+        except Exception as other:
+            assert False
+
+    class UpperCaseException(Exception):
+        pass
+
+    def raiseExceptionIfUpperCase(self, words):
+        for word in words:
+            if word.isupper():
+                raise self.UpperCaseException(word)
+    
+    def test_exception_upper_case(self):
+        words = ['tanaka', 'yasui', 'iwamoto', 'OKAMOTO']
+        try:
+            self.raiseExceptionIfUpperCase(words)
+            assert False
+        except self.UpperCaseException as err:
+            pass
+        except Exception as other:
+            assert False
+        
 if __name__ == '__main__':
-    unittest.main()
+    ut.main()
