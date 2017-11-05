86d3ca3764aec970da857251527023a22237da3f
ogawa-yu <badm.y.ogawa@gmail.com>
2017-11-06 00:06:46 +0900

    Python: テンプレートメソッドっぽく

--- Python/Starting/chap6/src/car.py
+++ Python/Starting/chap6/src/car.py
@@ -1,11 +1,12 @@
 class Car:
     def exclaim(self):
-        return "I'm car"
+        return "I'm " + self._name()
+    def _name(self):
+        return 'car'
 
 class Yugo(Car):
-    def exclaim(self):
-        return "I'm yugo"
-
+    def _name(self):
+        return 'yugo'
 
 if __name__ == '__main__':
     car = Car()
