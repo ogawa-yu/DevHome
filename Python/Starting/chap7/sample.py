57777223d86d78e537bcf0f06192b0eb626f30ae
ogawa-yu <badm.y.ogawa@gmail.com>
2017-11-12 22:50:47 +0900

    Python: binary dataを扱う File作成

---
+++ Python/Starting/chap7/sample.py
@@ -0,0 +1,6 @@
+class Sample:
+    def __init__(self, data):
+        self.__data = data
+    
+    def get_data(self):
+        return self.__data
--- Python/Starting/chap7/test_byte_data.py
+++ Python/Starting/chap7/test_byte_data.py
@@ -1,5 +1,6 @@
 import unittest as ut
 import struct
+import pickle
 
 class TestByteData(ut.TestCase):
     def test_struct_endian_unsigned_short(self):
@@ -17,6 +18,10 @@ class TestByteData(ut.TestCase):
     def test_struct_endian_unsigned_long_long(self):
         self.assertEqual(struct.pack('>Q', 154), b'\x00\x00\x00\x00\x00\x00\x00\x9a')
         self.assertEqual(struct.pack('<Q', 154), b'\x9a\x00\x00\x00\x00\x00\x00\x00')
+
+    def test_struct_sample(self):
+        import sample as s
+        self.assertEqual(struct.pack('>L', s.Sample(10)), '')
 
 if __name__ == '__main__':
     ut.main()
