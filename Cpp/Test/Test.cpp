72a63bc1fc1336e2439c72d12d09298e50369d88
ogawa-yu <badm.y.ogawa@gmail.com>
2017-11-27 00:58:15 +0900

    不要ディレクトリを削除

--- Cpp/Test/Test.cpp
+++
@@ -1,9 +0,0 @@
-#include <memory>
-#include <utility>
-#include <iostream>
-
-int main() {
-    std::unique_ptr<int> test = std::make_unique(100);
-    std::cout << test.get() << std::endl;
-    return 0;
-}
