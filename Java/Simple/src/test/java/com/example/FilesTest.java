58bdd23cd010aebac03e581a14d731c2d334b339
ogawa-yu <badm.y.ogawa@gmail.com>
2018-01-16 00:43:04 +0900

    Java: ファイル種別を正規表現で抽出

--- Java/Simple/src/test/java/com/example/FilesTest.java
+++ Java/Simple/src/test/java/com/example/FilesTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import java.util.regex.*;
 import java.nio.file.*;
 import java.io.File;
 import java.util.*;
@@ -10,13 +11,37 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 public class FilesTest {
+    private final Pattern IMAGE_FILE = Pattern.compile("(.*).(img|png|bmp)");
+    private final Pattern TEXT_FILE = Pattern.compile("(.*).(txt|log)");
+
     @Test
     public void fileFindTest() throws Exception {
         Path cp = Paths.get(FilesTest.class.getResource("./").toURI());
         Stream<Path> fs = Files.find(cp, 5,
             (path, attr) -> {
-                return attr.isRegularFile() && path.toString().endsWith("txt");
+                return attr.isRegularFile() && IMAGE_FILE.matcher(path.toString()).matches();
             });
-        assertThat(fs.count(), is(2L));
+        assertThat(fs.count(), is(1L));
+    }
+
+    @Test
+    public void fileFindTestDepthOne() throws Exception {
+        Path cp = Paths.get(FilesTest.class.getResource("./").toURI());
+        Stream<Path> fs = Files.find(cp, 1,
+            (path, attr) -> {
+                return attr.isRegularFile() && TEXT_FILE.matcher(path.toString()).matches();
+            });
+        assertThat(fs.count(), is(1L));
+    }
+
+    @Test
+    public void fileFindTestDepthZero() throws Exception {
+        Path cp = Paths.get(FilesTest.class.getResource("./").toURI());
+        Stream<Path> fs = Files.find(cp, 0,
+            (path, attr) -> {
+                System.out.println(path.toString());
+                return attr.isRegularFile() && IMAGE_FILE.matcher(path.toString()).matches();
+            });
+        assertThat(fs.count(), is(0L));
     }
 }
