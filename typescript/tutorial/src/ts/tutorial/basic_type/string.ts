15edd3f6951fcb4cbbe03fccb1d955c3a2dd031c
ogawa-yu <badm.y.ogawa@gmail.com>
2018-02-24 14:54:37 +0900

    TypeScript: データ型(string) バッククォートの場合だけテンプレート文字列が使えるので注意

---
+++ typescript/tutorial/src/ts/tutorial/basic_type/string.ts
@@ -0,0 +1,8 @@
+const color: string = "blue";
+console.log(color);
+
+let userName: string = 'user';
+let age = 27;
+let text: string = `${userName} will be ${ age + 1 } years old in this year.`
+
+console.log(text);
