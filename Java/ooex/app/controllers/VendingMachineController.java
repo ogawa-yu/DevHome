08d5b76629c43ac135a8aa21efa8654000a08ef9
ogawa-yu <badm.y.ogawa@gmail.com>
2018-02-24 18:12:20 +0900

    OOエクササイズ: Actorの受信応答処理を抽出

--- Java/ooex/app/controllers/VendingMachineController.java
+++ Java/ooex/app/controllers/VendingMachineController.java
@@ -1,35 +1,27 @@
 package controllers;
 
 import akka.actor.ActorRef;
-import akka.pattern.Patterns;
-import akka.util.Timeout;
-import javax.inject.Inject;
-import javax.inject.Named;
-
 import com.fasterxml.jackson.databind.JsonNode;
+import model.actor.Messaging;
 import model.vending.message.AllDrinks;
 import model.vending.message.Buy;
 import model.vending.message.Drink;
-import model.vending.message.Refund;
 import model.vending.message.Money;
+import model.vending.message.Refund;
 import modules.ActorModule;
 import play.libs.Json;
-import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
-import scala.concurrent.Await;
-import scala.concurrent.duration.Duration;
-import scala.concurrent.duration.FiniteDuration;
-import scala.reflect.ClassTag;
-
-import java.util.concurrent.TimeUnit;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 public class VendingMachineController extends Controller{
-    @Inject @Named(ActorModule.VENDING_MACHINE_ACTOR)
-    private ActorRef vendingMachine_;
+    @Inject
+    @Named(ActorModule.VENDING_MACHINE_ACTOR)
+    ActorRef vendingMachine_;
 
     public Result allDrinks() throws Exception {
-        return ok(Json.toJson(wait(Object.class, new AllDrinks())));
+        return ok(Json.toJson(Messaging.wait(Object.class, new AllDrinks(), vendingMachine_)));
     }
 
     public Result buy() throws Exception {
@@ -38,22 +30,10 @@ public class VendingMachineController extends Controller{
             return badRequest("Expecting Json data");
         }
         Buy body = Buy.of(json.findPath("drinkType").intValue(), json.findPath("amount").intValue());
-        return ok(Json.toJson(wait(Drink.class, body)));
+        return ok(Json.toJson(Messaging.wait(Drink.class, body, vendingMachine_)));
     }
 
     public Result refund() throws Exception {
-        return ok(Json.toJson(wait(Money.class, new Refund())));
-    }
-
-    private Object wait(Class<?> clazz, Object message) throws Exception {
-        FiniteDuration duration = Duration.create(5, TimeUnit.SECONDS);
-        return Await.result(
-                Patterns.ask(vendingMachine_, message, new Timeout(duration))
-                        .mapTo(new ClassTag() {
-                            @Override
-                            public Class<?> runtimeClass() {
-                                return clazz;
-                            }}),
-                duration);
+        return ok(Json.toJson(Messaging.wait(Money.class, new Refund(), vendingMachine_)));
     }
 }
