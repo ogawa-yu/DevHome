bccdd4f11bc4f6d8b96bf7d2210e31404d047bac
ogawa-yu <badm.y.ogawa@gmail.com>
2018-03-28 22:27:21 +0900

    Python: 遅い2重ループ

---
+++ Python/Starting/chapxx/bad_loop.py
@@ -0,0 +1,19 @@
+from numpy import arange
+import time
+
+def sum2d(arr):
+    M, N = arr.shape
+    result = 0.0
+
+    for i in range(M):
+        for j in range(N):
+            result += arr[i, j]
+
+    return result
+
+start = time.time()
+a = arange(100000000).reshape(10000, 10000)
+
+print(sum2d(a))
+elapsed_time = time.time() - start
+print("erapsed time:{0}[sec]".format(elapsed_time))
