c80de15ed96f003734bd4450bb0c177c47a46441
ogawa-yu <badm.y.ogawa@gmail.com>
2018-04-14 00:05:57 +0900

    :new: デコレータを複数付加する

--- Python/Starting/chapxx/deco.py
+++ Python/Starting/chapxx/deco.py
@@ -4,14 +4,24 @@ v = int(sys.argv[1])
 
 def G4(f):
     def exec():
-        if v <= 4:
-            print('not greater than 4')
+        if 4 < v:
+            print('greater than 4')
+            return
+        f()
+    return exec
+
+
+def L4(f):
+    def exec():
+        if  v < 4:
+            print ('less than 4')
             return
         f()
     return exec
 
 
 @G4
+@L4
 def func():
     print('work func()')
 
