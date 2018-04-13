af6b9ab7b280102995d48cd418bde414958b435f
ogawa-yu <badm.y.ogawa@gmail.com>
2018-04-14 00:07:46 +0900

    デコレータで戻り値を返す

--- Python/Starting/chapxx/deco.py
+++ Python/Starting/chapxx/deco.py
@@ -5,25 +5,23 @@ v = int(sys.argv[1])
 def G4(f):
     def exec():
         if 4 < v:
-            print('greater than 4')
-            return
-        f()
+            return 'greater than 4'
+        return f()
     return exec
 
 
 def L4(f):
     def exec():
         if  v < 4:
-            print ('less than 4')
-            return
-        f()
+            return 'less than 4'
+        return f()
     return exec
 
 
 @G4
 @L4
 def func():
-    print('work func()')
+    return 'work func()'
 
-func()
+print(func())
 
