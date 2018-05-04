6173d6b32287c0f4b36adc5f38b2cf91b2782145
ogawa-yu <badm.y.ogawa@gmail.com>
2018-05-04 22:38:50 +0900

    Python: MetaClassと通常Classのinit呼び出しタイミングの違いを出力

--- Python/MetaProgramming/SampleMeta1.py
+++ Python/MetaProgramming/SampleMeta1.py
@@ -1,6 +1,9 @@
 
 class SampleMeta(type):
     def __init__(cls, name, bases, namespec):
+        ''' __init__ is called instantiate cls at the first time.
+        '''
+        print("init metaclass", name)
         super(SampleMeta, cls).__init__(name, bases, namespec)
         cls.uses_metaclass = lambda self: "Yes!"
 
@@ -8,6 +11,10 @@ class SampleMeta(type):
 class Sample(metaclass=SampleMeta):
     # for Python 2.x statements
     # __metaclass__ = SampleMeta
+
+    def __init__(self):
+        print("init", self.__class__)
+
     def foo(self):
         pass
 
@@ -16,7 +23,26 @@ class Sample(metaclass=SampleMeta):
         pass
 
 
+class Sample2(metaclass=SampleMeta):
+    def __init__(self):
+        print("init", self.__class__)
+
+
 if __name__ == '__main__':
-    s = Sample()
-    print([m for m in dir(s) if not m.startswith('__')])
-    print(s.uses_metaclass())
+    s1 = Sample()
+    ss = Sample()
+    s2 = Sample2()
+    print([m for m in dir(s1) if not m.startswith('__')])
+    print([m for m in dir(s2) if not m.startswith('__')])
+    print(s1.uses_metaclass())
+
+""" Outputs:
+init metaclass Sample
+init metaclass Sample2
+init <class '__main__.Sample'>
+init <class '__main__.Sample'>
+init <class '__main__.Sample2'>
+['bar', 'foo', 'uses_metaclass']
+['uses_metaclass']
+Yes!
+"""
