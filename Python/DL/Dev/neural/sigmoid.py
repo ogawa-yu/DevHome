37e8bfdd5cfdf24e1503a46afa541f42e4018455
ogawa-yu <badm.y.ogawa@gmail.com>
2018-05-14 04:07:25 +0900

    plot処理をmainに限定

--- Python/DL/Dev/neural/sigmoid.py
+++ Python/DL/Dev/neural/sigmoid.py
@@ -3,3 +3,10 @@ import numpy as np
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+if __name__ == '__main__':
+    x = np.arange(-10.0, 10.0, 0.1)
+    y = sigmoid(x)
+    plt.plot(x, y)
+    plt.ylim(-0.1, 1.1)
+    plt.show()
