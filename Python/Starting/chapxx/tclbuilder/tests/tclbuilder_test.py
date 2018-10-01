6c6d41c291c665850a7e07bd424b89f76730d2ef
ogawa-yu <badm.y.ogawa@gmail.com>
2018-10-01 22:39:45 +0900

    join

--- Python/Starting/chapxx/tclbuilder/tests/tclbuilder_test.py
+++ Python/Starting/chapxx/tclbuilder/tests/tclbuilder_test.py
@@ -35,16 +35,14 @@ class TestTclBuilder(object):
             "set param2_attr2s [list 4 0 0]",
             "",
             "foreach m $materials p1a1 $param1_attr1s p1a2 $param1_attr2s {",
-            "    set l [list $m \":\" $p1a1 \",\" $p1a2]",
+            "    set l [join [list $m \":\" $p1a1 \",\" $p1a2]]",
             "    puts $l",
-            "    unset l",
             "}",
             "",
             "foreach mo $materials p2a1 $param2_attr1s {",
             "    foreach mi $materials p2a2 $param2_attr2s {",
-            "        set m_prod [list $mo \"+\" $mi \"+\" $p2a1 \"=\" $mi \"+\" $mo \"+\" $p2a2]",
+            "        set m_prod [join [list $mo \"+\" $mi \"+\" $p2a1 \"=\" $mi \"+\" $mo \"+\" $p2a2]]",
             "        puts $m_prod",
-            "        unset m_prod",
             "    }",
             "}"
         ]
