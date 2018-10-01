cf52df8bca67bb250e188aec474bfae68085d94d
ogawa-yu <badm.y.ogawa@gmail.com>
2018-10-01 21:59:13 +0900

    パース処理の実装

--- Python/Starting/chapxx/tclbuilder/tclbuilder/tclbuilder.py
+++ Python/Starting/chapxx/tclbuilder/tclbuilder/tclbuilder.py
@@ -1,7 +1,11 @@
 
+import re
+from functools import reduce
 from pathlib import Path
 
 class TclBuilder(object):
+    EXP_PLACE_HOLDER = re.compile("%[\\S]+%")
+
     def build(self, params):
         command_type = params["params"]["type"]
         founds = list(Path("./command/").glob(
@@ -12,4 +16,38 @@ class TclBuilder(object):
         command_file: Path = founds[0]
         with command_file.open("r") as f:
             lines = f.readlines()
+        result = list()
+        for l in lines:
+            result.extend(self.parse_script(l, params["params"]))
+        with Path("./result.tcl").open("w") as fw:
+            fw.writelines(result)
+        return result
+
+    def parse_script(self, line: str, params: dict):
+        ms = self.EXP_PLACE_HOLDER.finditer(line)
+        lines = list()
+        ms = list(ms)
+        if ms is None or len(ms) == 0:
+            lines.append(line)
+            return lines
+        for m in ms:
+            key = m.group()
+            if not key[1:-1] in params:
+                raise ValueError(
+                    "{} is not found in params. {}".format(key, params))
+            value = params[key[1:-1]]
+            if isinstance(value, list):
+                value_str = TclBuilder.to_tcl_list(value)
+            else:
+                value_str = str(params[key])
+            lines.append(re.sub(key, value_str, line))
         return lines
+
+    @staticmethod
+    def define_tcl_list(key, values):
+        return "set {} [list {}]".format( 
+            key, TclBuilder.to_tcl_list(values))
+
+    @staticmethod
+    def to_tcl_list(values):
+        return reduce(lambda l, r: str(l) + " " + str(r), values)
--- Python/Starting/chapxx/tclbuilder/tests/tclbuilder_test.py
+++ Python/Starting/chapxx/tclbuilder/tests/tclbuilder_test.py
@@ -7,9 +7,10 @@ class TestTclBuilder(object):
     @pytest.fixture(scope="function", autouse=True)
     def input(self):
         return {
-            "materials": ["/org/", "sil", "ox"],
+            "materials": ["/org/", "/sil/", "/ox/"],
             "params": {
                 "type": "seq1",
+                "materials": ["/org/", "/sil/", "/ox/"],
                 "param1": 100,
                 "materials.param1.attr1": [1, 0, 1],
                 "materials.param1.attr2": [2, 0, 0],
@@ -23,19 +24,20 @@ class TestTclBuilder(object):
         actual = testee.build(input)
         actual = list(map(lambda l: l[:-1], actual)) # remove \n
         expected = [
-            "set materials [list %materials%]",
+            "set materials [list /org/ /sil/ /ox/]",
             "",
-            "set param1_attr1s [list %materials.param1.attr1%]",
+            "set param1_attr1s [list 1 0 1]",
             "",
-            "set param1_attr2s [list %materials.param1.attr2%]",
+            "set param1_attr2s [list 2 0 0]",
             "",
-            "set param2_attr1s [list %materials.param2.attr1%]",
+            "set param2_attr1s [list 3 0 0]",
             "",
-            "set param2_attr2s [list %materials.param2.attr2%]",
+            "set param2_attr2s [list 4 0 0]",
             "",
             "puts $param1_attr1s",
             "puts $param1_attr2s",
             "puts $param2_attr1s",
             "puts $param2_attr2s"
         ]
+        print("A", actual)
         assert actual == expected
