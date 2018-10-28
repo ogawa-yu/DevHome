6e1877bf0974de3152eea609cb95ab9700310d26
ogawa-yu <badm.y.ogawa@gmail.com>
2018-10-28 14:31:05 +0900

    Python: 線・テキスト描画をmatplotlibで処理する

--- Python/Starting/chapxx/opencv/ch1/read.py
+++ Python/Starting/chapxx/opencv/ch1/read.py
@@ -6,12 +6,10 @@ print(type(img)) # <class 'numpy.ndarray'>
 print(img.shape) # (222, 180, 3)
 
 begin_point = (0, 80)
-end_point = (180, 80)
-color = (255, 255, 0)
+color = "yellow"
 text = "Example"
-font = cv2.FONT_HERSHEY_PLAIN
-font_size = 0.8
-img = cv2.line(img, begin_point, end_point, color, 1)
-cv2.putText(img, text, begin_point, font, font_size, color, 1, cv2.LINE_4)
 plt.imshow(img)
+
+plt.axhline(begin_point[1], color=color, linestyle="--", linewidth=1)
+plt.text(*begin_point, text, color=color)
 plt.show()
